58ad0e9d8e1883c8b1a83d2104fc1b0967e049c2
LinkingMx <armando.reyes@live.com.mx>
2025-08-07 19:45:25 -0600

    Initial Commit

---
+++ app/Filament/Resources/DocumentationResource/Pages/CreateDocumentation.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\DocumentationResource\Pages;
+
+use App\Filament\Resources\DocumentationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDocumentation extends CreateRecord
+{
+    protected static string $resource = DocumentationResource::class;
+    
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Asegurar que el documento se crea en draft y con el usuario actual
+        $data['status'] = 'draft';
+        $data['created_by'] = auth()->id();
+        
+        return $data;
+    }
+    
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+    
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Documento creado y enviado para aprobación';
+    }
+}
